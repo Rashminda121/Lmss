e9224cf846a013ea6567edd831c0bbe21a1cfb14
Jayamuni Rashminda <rashminda121@gmail.com>
2025-05-04 18:08:53 +0530

    feat: github workflows

--- frontend/components/course-card.tsx
+++ frontend/components/course-card.tsx
@@ -31,10 +31,10 @@ export const CourseCard = ({
           <Image fill className="object-contain" alt={title} src={imageUrl} />
         </div>
         <div className="flex flex-col pt-2">
-          <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transistion line-clamp-2">
+          <div className="text-lg md:text-base font-medium group-hover:text-sky-700 transition overflow-y-auto max-h-16">
             {title}
           </div>
-          <p className="text-xs text-muted-foreground">{category}</p>
+          <p className="pt-[5px] text-xs text-muted-foreground">{category}</p>
           <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
             <div className="flex items-center gap-x-1 text-slate-500">
               <IconBadge size="sm" icon={BookOpen} />
