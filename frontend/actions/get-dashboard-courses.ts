81c2d15e412ad553d085268df11a6feb7afa36f4
Jayamuni Rashminda <rashminda121@gmail.com>
2025-05-05 15:27:45 +0530

    refactor: prisma files

--- frontend/actions/get-dashboard-courses.ts
+++ frontend/actions/get-dashboard-courses.ts
@@ -1,5 +1,5 @@
 import { db } from "@/lib/db";
-import { category, Chapter, Course } from "@prisma/client";
+import { Category, Chapter, Course } from "@prisma/client";
 import { getProgress } from "@/actions/get-progress";
 
 type CourseWithProgressWithCategory = Course & {
